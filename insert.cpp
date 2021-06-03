a2b723262611e1c8868c0afcdf7a1f3a81fbd9e8
Vanshika-2001 <75316523+Vanshika-2001@users.noreply.github.com>
2021-06-03 21:42:17 +0530

    changes

---
+++ insert.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+struct array
+{
+    int a[10];
+    int size;
+    int length;
+};
+
+void display (int a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    diplay (a)
+    return 0;
+}
---
+++ pointers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int m, n;
+int * ptrInt;
+int ** ptrPtrInt;
+ptrPtrInt = &ptrInt;
+cout << "Give m and n: "; cin >> m >> n;
+ptrInt = &n; cout << *(*ptrPtrInt) << endl;
+ptrInt = &m; cout << *(*ptrPtrInt) << endl;
+return 0;
+}
