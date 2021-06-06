93167815d875560ca566e8f0e10577a26f94f30e
Vanshika-2001 <75316523+Vanshika-2001@users.noreply.github.com>
2021-06-06 11:17:21 +0530

    changes

---
+++ arrdelete.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+struct Array
+{
+ int A[10];
+ int size;
+ int length;
+};
+ void Display(struct Array arr)
+ {
+ cout<<"Elements are"<<endl;
+ for(int i=0;i<arr.length;i++)
+ cout<<arr.A[i]<<" ";
+ }
+ int Delete(struct Array *arr,int index)
+ {
+     int x=0;
+     int i;
+     if(index>=0 && index<arr->length)
+     {
+ x=arr->A[index];
+ for(i=index;i<arr->length-1;i++)
+     arr->A[i]=arr->A[i+1];
+     arr->length--;
+     return x;
+ }
+ return 0;
+ }
+ 
+ 
+int main()
+{
+struct Array arr1={{2,3,4,5,6},10,5};
+Delete(&arr1,4);
+Display(arr1);
+ return 0;
+}
